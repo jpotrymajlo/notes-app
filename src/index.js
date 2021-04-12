974aef5141291f376cab3b6d41400848a1136b0e
Jacek Potrymajlo <>
2021-04-13 00:03:48 +0200

    fixed AWS configuration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
+import config from "./config"
 
 Amplify.configure({
   Auth: {
