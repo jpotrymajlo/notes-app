b932a110b0b92b6a7e77e100aa87d34c34620e29
Jacek Potrymajlo <>
2021-04-13 19:06:49 +0200

    Login and logout added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes"
 import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import Auth from "@aws-amplify/auth";
+import { useHistory } from "react-router-dom"
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const history = useHistory();
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    history.push("/login")
+  }
+
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -18,16 +30,23 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
+            {isAuthenticated
+            ? <NavItem onClick={handleLogout}>Loguot</NavItem>
+            : <>
             <LinkContainer to="/signup">
               <NavItem>Signup</NavItem>
             </LinkContainer>
             <LinkContainer to="/login">
               <NavItem href="/login">Login</NavItem>
             </LinkContainer>
+             </> 
+             }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes/>
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
