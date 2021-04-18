eee117c516fbec86133bfe77095e75745a5cf814
Jacek Potrymajlo <>
2021-04-18 22:16:42 +0200

    Revert "Provided config values from environment"
    :q
    
    This reverts commit 76f10d2ef4618bec958558c79e3ad87dbf97a5f9.

--- src/index.js
+++ src/index.js
@@ -10,22 +10,22 @@ import config from "./config"
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
-    region: process.env.REGION,
-    userPoolId: process.env.COGNITO_USER_POOL_ID,
-    identityPoolId: process.env.COGNITO_IDENTITY_POOL_ID,
-    userPoolWebClientId: process.env.COGNITO_APP_CLIENT_ID
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
   },
   Storage: {
-    region: process.env.REGION,
-    bucket: process.env.S3_BUCKET,
-    identityPoolId: process.env.COGNITO_IDENTITY_POOL_ID
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
   },
   API: {
     endpoints: [
     {
       name: "notes",
-      endpoint: process.env.API_GATEWAY_URL,
-      region: process.env.REGION
+      endpoint: config.apiGateway.URL,
+      region: config.apiGateway.REGION
     },
     ]
     }
